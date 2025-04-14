e5bff3107cf7f971a38397bbde61cee64df626fa
eliya-mortal <eliyaporooo@gmail.com>
2025-04-14 03:18:46 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-
 import asyncio
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from analyzer_bot import run_analysis_all, send_daily_reports
+from telegram.ext import Application, CommandHandler
 from handlers import (
-    start,
-    get_report,
-    get_5_0,
-    get_karakter_match,
-    get_odd_stats,
+    start, get_report, get_5_0, get_karakter_match, get_odd_stats,
+    help_command, about_command
 )
 from config import BOT_TOKEN as TOKEN
 
 
-async def loop_task():
+async def loop_task(bot):
     while True:
         await run_analysis_all(bot)
         await asyncio.sleep(300)  # هر 5 دقیقه
 
 
-async def daily_task():
+async def daily_task(bot):
     while True:
         now = asyncio.get_event_loop().time()
         target = ((24 * 60 * 60) - 60)  # ساعت 23:59
@@ -34,21 +28,19 @@ async def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("report", get_report))
     app.add_handler(CommandHandler("getfile_5_0", get_5_0))
-
     app.add_handler(CommandHandler("getfile_karaktermatch", get_karakter_match))
     app.add_handler(CommandHandler("getfile_oddstats", get_odd_stats))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("about", about_command))
 
-    # اجرای تسک‌های جداگانه
-    asyncio.create_task(loop_task())
-    asyncio.create_task(daily_task())
+    # اجرای تسک‌های جداگانه با bot
+    bot = app.bot
+    asyncio.create_task(loop_task(bot))
+    asyncio.create_task(daily_task(bot))
 
     # شروع ربات
     await app.run_polling()
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except RuntimeError:
-        loop = asyncio.get.event_loop()
-        loop.run_until_complete(main())
+    asyncio.run(main())
